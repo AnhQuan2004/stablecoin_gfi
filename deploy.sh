e2a47e496f01892e706fdb4db05306662fb949a7
AnhQuan2004 <anhquan200304@gmail.com>
2025-04-22 17:55:22 +0700

    add binance and nft, fix bug

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker build -t stablecoin_gfi .
-docker tag stablecoin_gfi jasong03/stablecoin_gfi:latest
-docker push jasong03/stablecoin_gfi:latest
+docker build -t coin_gfi .
+docker tag coin_gfi jasong03/coin_gfi:latest
+docker push jasong03/coin_gfi:latest
